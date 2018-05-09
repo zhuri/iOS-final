ae055e94cf9202283f9fb38baf165b802655a53f
Cacttus Education <vetonxh@cacttus.com>
2018-05-09 11:51:17 +0200

    Initial Commit

---
+++ GrupiX-Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrupiX-Final
+//
+//  Created by Cacttus Education 12 on 5/9/18.
+//  Copyright © 2018 Cacttus Education 12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
