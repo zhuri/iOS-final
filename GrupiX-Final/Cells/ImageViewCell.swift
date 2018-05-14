3d86f4f3eea496aed6052490ae67ae2f2c3dfbaf
Cacttus Education 12 <cacttuseducation12@Cacttuss-iMac-7.local>
2018-05-14 18:39:42 +0200

    first commit

---
+++ GrupiX-Final/Cells/ImageViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageViewCell.swift
+//  GrupiX-Final
+//
+//  Created by Cacttus Education 12 on 5/14/18.
+//  Copyright © 2018 Cacttus Education 12. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewCell: UICollectionViewCell {
+    
+}
---
+++ GrupiX-Final/Cells/LocationCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationCell.swift
+//  GrupiX-Final
+//
+//  Created by Cacttus Education 12 on 5/9/18.
+//  Copyright © 2018 Cacttus Education 12. All rights reserved.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
